06196a3bbad5e5b1f85ced45a9779fc853024824
OnemaYepes <jumayemo108@gmail.com>
2026-04-26 18:16:54 -0500

    configurando karma

--- frontend-angular/karma.conf.js
+++ frontend-angular/karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function (config) {
     customLaunchers: {  
       ChromeHeadlessCI: {  
         base: 'ChromeHeadless',  
-        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],  
+        flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],  
       },  
     },  
     restartOnFileChange: false,
